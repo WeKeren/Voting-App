4aa3ead881fa854708515cc0688007e8b9cea207
WeKeren <keren.kwg@gmail.com>
2022-09-12 13:18:27 +0300

    Job Interview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			languages : [
+				{name: "Keren Weisberg", votes: 0},
+				{name: "Different Contestant", votes: 0},
+			]
+		}
+	}
 
+	vote (i) {
+		let newLanguages = [...this.state.languages];
+		newLanguages[i].votes++;
+	
+		this.setState({languages: newLanguages});
+		
+	}
+
+	render(){
+		return(
+			<>
+<h1>Vote Your Candidate for Cloud Portal Devops Operations (Student)</h1>
+<div className="Contender">
+					{
+						this.state.languages.map((lang, i) => 
+		<div key={i} className="Contender">
+		<div className="voteCount">
+									{lang.votes}
+								</div>
+<div className="Contender">
+								{lang.name}
+								</div>
+								<button onClick={this.vote.bind(this, i)}>Click Here</button>
+	</div>
+	)}
+	</div>
+        </>
+	);
+	}
+}
 export default App;
